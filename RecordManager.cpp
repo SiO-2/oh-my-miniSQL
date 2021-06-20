deb1e3583cdcbd5040b534a1977dec1db55a8dac
SiO-2 <2586457570@qq.com>
2021-06-20 14:57:14 +0800

    更新recordmanager

--- RecordManager.cpp
+++ RecordManager.cpp
@@ -1,35 +1,68 @@
 #include "RecordManager.h"
 
-//创建一个新的表
-bool RecordManager::CreateTableFile(const string &tablename)
+/*
+    函数功能：创建一个表元数据文件以及一个表数据文件
+    传入参数：表名称
+    返回值：没有返回值
+*/
+void RecordManager::CreateTableFile(const Table &table)
 {
-    string filename = FILEPATH + tablename + TABLE_SUFFIX;
+    string tablename = table.m_metadata.name;
+    string filename_data = GetDataFileName(tablename);
+    string filename_meta = GetMetaFileName(tablename);
     FILE *fp;
-    if ((fp = fopen(filename.c_str(), "wb+")) == NULL)
+    if ((fp = fopen(filename_data.c_str(), "wb+")) == NULL)
     {
-        printf("Can't create %s\n", table);
+        printf("Can't create %s\n", tablename);
         exit(EXIT_FAILURE);
     }
     fclose(fp);
+    if ((fp = fopen(filename_meta.c_str(), "wb+")) == NULL)
+    {
+        printf("Can't create %s\n", tablename);
+        exit(EXIT_FAILURE);
+    }
+    
 }
 
-//删除表
-bool RecordManager::DropTableFile(const string &tablename){
-    string filename=FILEPATH + tablename + TABLE_SUFFIX;
-    FlushBlock(filename);
-    remove(filename.c_str());
+/*
+    函数功能：删除表文件
+    传入参数：表名称
+    返回值：没有返回值
+*/
+void RecordManager::DropTableFile(const Table &table)
+{
 }
 
-//插入记录，支持每次一条记录的插入操作
-bool RecordManager::InsertTuple(const string &tablename, const Tuple &tuple){
-    string filename=FILEPATH + tablename + TABLE_SUFFIX;
-    for()
-    BID bid=ReadFile2Block(filename)
-
+/*
+    函数功能：向表文件中插入元组，支持每次一条元组的插入操作
+    传入参数：表名称，元组变量
+    返回值：没有返回值
+*/
+void RecordManager::InsertTuple(const string &tablename, const Tuple &tuple)
+{
+    string filename = DATA_FILE_PATH + tablename + TABLE_SUFFIX;
+    unsigned int file_size = bmanager->GetFileSize(filename);
+    vector<unsigned int> block_offset;
+    vector<unsigned int> tuple_offset;
+    block_offset.push_back(file_size / BLOCKSIZE);
+    tuple_offset.push_back(file_size % BLOCKSIZE);
+    vector<BID> bids(bmanager->ReadFile2Block(filename, block_offset));
+    //由于插入时最多插入一条元组，所以不需要遍历
+    char *tuple_add = bmanager->blocks[*bids.begin()].data + *tuple_offset.begin();
+    // memcpy(tuple_add, );
 }
 
-//数据查询，可以通过指定用and 连接的多个条件进行查询，支持等值查询和区间查询
-bool RecordManager::SelectTuple(const string &tablename, const vector<string> &attribute = NULL, const vector<ConditionUnit> &condition = NULL) const;
+/*
+    函数功能：数据查询，可以通过指定用and 连接的多个条件进行查询，支持等值查询和区间查询
+*/
+bool RecordManager::SelectTuple(const string &tablename, const vector<string> &attribute = vector<string>(), const vector<ConditionUnit> &condition = vector<ConditionUnit>()) const
+{
+}
 
-//删除元组，支持每次一条或多条记录的删除操作
-bool RecordManager::DelectTuple(const string &tablename, const vector<ConditionUnit> &condition = NULL);
+/*
+    函数功能：删除元组，支持每次一条或多条记录的删除操作
+*/
+bool RecordManager::DelectTuple(const string &tablename, const vector<ConditionUnit> &condition = vector<ConditionUnit>())
+{
+}
--- RecordManager.h
+++ RecordManager.h
@@ -9,36 +9,81 @@
 using namespace std;
 
 //数据文件的存放目录
-const string FILEPATH= ".database/data/";
+const string TABLE_PATH = ".database/data/";
+const string INDEX_PATH = ".database/index/";
+const string META_PATH = ".database/meta/";
 
 //文件后缀名
-const string TABLE_SUFFIX=".table";
-const string INDEX_SUFFIX=".index";
+const string TABLE_SUFFIX = ".table";
+const string INDEX_SUFFIX = ".index";
+const string META_SUFFIX = ".meta";
 
 class RecordManager
 {
 private:
 	BufferManager *bmanager;
-	IndexManager *imanager;
+	// IndexManager *imanager;
 
 public:
 	RecordManager(){};
 	~RecordManager(){};
 
-	//创建一个新的表
-	bool CreateTableFile(const string &tablename);
+	/*
+		函数功能：根据表名得到存储表数据的文件名
+		传入参数：表名
+		返回值：存储表数据文件的文件名
+	*/
+	string GetDataFileName(const string tablename) const
+	{
+		return TABLE_PATH + tablename + TABLE_SUFFIX;
+	}
 
-	//删除表
-	bool DropTableFile(const string &tablename);
+	/*
+		函数功能：根据表名得到存储表index的文件名
+		传入参数：表名
+		返回值：存储表index文件的文件名
+	*/
+	string GetIndexFileName(const string tablename) const
+	{
+		return INDEX_PATH + tablename + INDEX_SUFFIX;
+	}
 
-	//插入记录，支持每次一条记录的插入操作
-	bool InsertTuple(const string &tablename, const vector<struct DataUnit> &tuple);
+	/*
+		函数功能：根据表名得到存储表元数据的文件名
+		传入参数：表名
+		返回值：存储表元数据文件的文件名
+	*/
+	string GetMetaFileName(const string tablename) const
+	{
+		return META_PATH + tablename + META_SUFFIX;
+	}
+
+	/*
+		函数功能：创建一个新的表文件
+		传入参数：表名称
+		返回值：没有返回值
+	*/
+	void CreateTableFile(const Table &table);
+
+	/*
+		函数功能：删除表文件
+		传入参数：表名称
+		返回值：没有返回值
+	*/
+	void DropTableFile(const Table &table);
+
+	/*
+		函数功能：向表文件中插入元组，支持每次一条元组的插入操作
+		传入参数：表名称，元组变量
+		返回值：没有返回值
+	*/
+	void InsertTuple(const string &tablename, const Tuple &tuple);
 
 	//数据查询，可以通过指定用and 连接的多个条件进行查询，支持等值查询和区间查询
-	bool SelectTuple(const Table &tablename, const vector<string> &attribute = NULL, const vector<ConditionUnit> &condition = NULL) const;
+	bool SelectTuple(const string &tablename, const vector<string> &attribute = vector<string>(), const vector<ConditionUnit> &condition = vector<ConditionUnit>()) const;
 
 	//删除元组，支持每次一条或多条记录的删除操作
-	bool DelectTuple(const Table &tablename, const vector<ConditionUnit> &condition = NULL);
+	bool DelectTuple(const string &tablename, const vector<ConditionUnit> &condition = vector<ConditionUnit>());
 };
 
 #endif //MINISQL_RECORDMANAGER_H
