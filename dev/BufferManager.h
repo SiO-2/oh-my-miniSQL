2dd8ed706892e32b89067207c62b88bbb1e2ac24
zrrr333 <62089005+zrrr333@users.noreply.github.com>
2021-06-23 10:48:40 -0500

    Add files via upload

---
+++ dev/BufferManager.h
@@ -0,0 +1,151 @@
+#ifndef MINISQL_BUFFERMANAGER_H
+#define MINISQL_BUFFERMANAGER_H
+#define DEBUG
+#include <iostream>
+#include <string>
+#include "MiniSQL.h"
+
+using namespace std;
+
+typedef unsigned int BID;
+const BID MAX_BLOCK_NUMBER = 4096;
+const unsigned int BLOCKSIZE = 4096;
+
+class Block
+{
+private:
+    string filename;
+    int offset; //对于文件而言的偏移地址
+    bool valid; //由于不需要考虑多线程，直接通过valid表示
+    bool dirty;
+
+public:
+    //只有data可以给其他manager修改
+    char data[BLOCKSIZE];
+
+    Block()
+    {
+        filename = "";
+        offset = -1;
+        valid = false;
+        dirty = false;
+    };
+    ~Block() {}
+
+    //大量的读取和设置函数主要是为了保护Block的相关信息不被修改，保证block的分配完全由buffermanager管理
+    string GetFilename() const
+    {
+#ifdef DEBUG
+        // cout << "GetFilename return filename = " << this->filename << "output finish" << endl;
+#endif
+        return this->filename;
+    }
+    int GetOffset() const
+    {
+        return this->offset;
+    }
+    bool IsValid() const
+    {
+#ifdef DEBUG
+        // cout << "BufferManager.h::IsValid():49" << endl;
+        // cout << "GetFilename return valid = " << this->valid << "   output finish" << endl;
+#endif
+        return this->valid;
+    }
+    bool IsDirty() const
+    {
+        return this->dirty;
+    }
+    void SetFilename(const string &filename)
+    {
+        this->filename = filename;
+    }
+    void SetOffset(const unsigned int &offset)
+    {
+        this->offset = offset;
+    }
+
+    //设置dirty与valid
+    void SetDirty()
+    {
+        this->dirty = true;
+    }
+    void SetUnDirty()
+    {
+        this->dirty = false;
+    }
+    void SetValid() //表示占用该block
+    {
+        this->valid = true;
+    }
+    void SetUnValid() //相当于释放这个block
+    {
+        this->valid = false;
+    }
+};
+
+class BufferManager
+{
+private:
+    /*
+        函数功能：进行BID的分配
+        传入参数：带路径的文件名，以及地址偏移
+        返回值：分配好的block的bid
+    */
+    BID GetBlock(const string &filename, const unsigned int &offset) const;
+
+    /*
+        函数功能：将block初始化
+        传入参数：block的bid，带路径的文件名，block在文件中的偏移
+        返回值：没有返回值
+    */
+    void SetBlockInfo(const BID &bid, const string &filename, const unsigned int &offset);
+
+public:
+    //record不能调用分配和管理block的函数
+
+    Block blocks[MAX_BLOCK_NUMBER];
+
+    //record可以调用的只有读文件和写文件
+
+    BufferManager(){};
+
+    ~BufferManager()
+    {
+        for (BID bid = 0; bid < MAX_BLOCK_NUMBER; bid++)
+            if (blocks[bid].IsDirty())
+                WriteBlock2File(bid);
+
+        cout << "BufferManager.h>BufferManager>~BufferManager() finished" << endl;
+    };
+
+    /*
+        函数功能：获取文件的大小
+        传入参数：带路径的文件名
+        返回值：文件大小，单位为Byte
+    */
+    size_t GetFileSize(const std::string &filename) const;
+
+    /*
+        函数功能：将文件读入到blocks中
+        传入参数：文件名以及block在文件中的偏移量（偏移量可缺省）
+        返回值：vector<BID> bids，即文件对应的所有的block的bid
+    */
+    vector<BID> ReadFile2Block(const string &filename, const vector<unsigned int> &offset = vector<unsigned int>());
+
+    /*
+        函数功能：将块写回文件
+        传入参数：传入参数为需要写回的块的bid
+        返回值：没有返回值
+    */
+    void WriteBlock2File(const BID &bid);
+
+    /*
+        函数功能：当表被删除时，将对应的block清空
+        传入参数：被删除文件的带路径文件名
+        返回值：没有返回值
+    */
+    void FlushBlock(const string &filename);
+};
+
+#endif //MINISQL_BUFFERMANAGER_H
---
+++ dev/MiniSQL.cpp
@@ -0,0 +1,263 @@
+#include <regex>
+#include "MiniSQL.h"
+
+TableMetadata::TableMetadata()
+{
+}
+
+TableMetadata::TableMetadata(string name, int attr_num, int primary_key, int primary_index)
+{
+    this->name = name;
+    this->attr_num = attr_num;
+    this->primary_key = primary_key;
+    this->primary_index = primary_index;
+}
+
+TableMetadata::TableMetadata(TableMetadata &t)
+{
+    this->attr_num = t.attr_num;
+    this->name = t.name;
+    this->primary_index = t.primary_index;
+    this->primary_key = t.primary_key;
+}
+
+void TableMetadata::Print()
+{
+    cout << "[Table Meta]:\n";
+    cout << "Name:" << this->name << endl;
+    cout << "Attr num:" << this->attr_num << endl;
+    cout << "Primary Key:" << this->primary_key << endl;
+    cout << "Primary Index:" << this->primary_index << endl;
+}
+
+Attribute::Attribute(string name, string typestr, bool notnull, bool unique, bool primary_key)
+{
+    std::regex re_char("char\\(\\d+\\)", regex_constants::icase);
+    std::regex re_int("int", regex_constants::icase);
+    std::regex re_float("float", regex_constants::icase);
+    std::regex re_num("\\d+", regex_constants::icase);
+    this->charlen = -1;
+    if (std::regex_match(typestr, re_int))
+    {
+        this->type = INT_UNIT;
+    }
+    else if (std::regex_match(typestr, re_float))
+    {
+        this->type = FLOAT_UNIT;
+    }
+    else if (std::regex_match(typestr, re_char))
+    {
+        this->type = CHAR_UNIT;
+        sregex_iterator itr1(typestr.begin(), typestr.end(), re_num);
+        sregex_iterator itr2;
+        for (sregex_iterator itr = itr1; itr != itr2; ++itr)
+        {
+            // ！！！ 这里可能有点隐患，默认\d+是贪婪匹配了
+            // string L = itr->str();
+            // cout<<L<<endl;
+            this->charlen = stoi(itr->str());
+            break;
+            // cout << itr->str() << endl;
+        }
+    }
+    this->name = name;
+    this->charlen = charlen;
+    this->notnull = notnull;
+    this->unique = unique;
+    this->primary_key = primary_key;
+    // cout<<"[info]: Create Attribute "<<(this->name)<<", type = "<<this->type<<", charlen = "<<this->charlen<<endl;
+    // cout<<"[debug]: "<<this->name.length()<<endl;
+}
+
+void Attribute::set_pk(bool pk)
+{
+    cout << "[debug]: call set pk for " << this->name << " to pk=" << pk << endl;
+    this->primary_key = pk;
+}
+
+void Attribute::Print()
+{
+    // cout<<"[debug]: "<<this->name.length()<<endl;
+    cout << "[info]: Attr:" << this->name << ", type:";
+    if (this->type == INT_UNIT)
+    {
+        cout << "int";
+    }
+    else if (this->type == FLOAT_UNIT)
+    {
+        cout << "float";
+    }
+    else if (this->type == CHAR_UNIT)
+    {
+        cout << "char(" << this->charlen << ")";
+    }
+    else
+    {
+        cout << "Invalid";
+    }
+    if (this->notnull)
+    {
+        cout << ", not null";
+    }
+    if (this->unique)
+    {
+        cout << ", unique";
+    }
+    if (this->primary_key)
+    {
+        cout << ", primary key";
+    }
+
+    cout << endl;
+}
+
+Table::Table(TableMetadata m_metadata, vector<Attribute> m_attribute) : m_metadata(m_metadata), m_attribute(m_attribute){};
+
+Table::Table(Table &table)
+{
+    this->m_metadata = table.m_metadata;
+    this->m_attribute.assign(table.m_attribute.begin(), table.m_attribute.end());
+}
+
+void Table::Print()
+{
+    cout << "[Table Info]:\n";
+    this->m_metadata.Print();
+    for (auto Attr : this->m_attribute)
+    {
+        Attr.Print();
+    }
+}
+
+Index::Index(Index &index)
+{
+    this->attr_num = index.attr_num;
+    this->index_name = index.index_name;
+    this->table = index.table;
+}
+
+Index::Index()
+{
+}
+
+void Index::Print()
+{
+    cout << "[Index Info]: index name = " << this->index_name << endl;
+}
+
+Unit::Unit()
+{
+}
+
+Unit::Unit(Value value, DataType datatype) : value(value)
+{
+    // this->value = value;
+    this->datatype = datatype;
+}
+
+void Unit::Print()
+{
+    cout << "[Unit info]: ";
+    string str;
+    switch (this->datatype)
+    {
+    case INT_UNIT:
+        cout << "int: " << this->value.int_value << endl;
+        break;
+    case FLOAT_UNIT:
+        cout << "float: " << this->value.float_value << endl;
+        break;
+    case CHAR_UNIT:
+        str = this->value.char_n_value;
+        cout << "string: " << str << endl;
+        break;
+    default:
+        break;
+    }
+}
+Tuple::Tuple() : tuple_value(), valid(true)
+{
+}
+
+void Tuple::Print()
+{
+    cout << "[Tuple info]:" << endl;
+    for (auto unit : this->tuple_value)
+    {
+        unit.Print();
+    }
+    cout << "[Tuple info end]" << endl;
+}
+// void DataUnit::Print(){
+//     cout<<"[DataUnit]: attrname = "<<this->attr_name<<", DataType = ";
+//     switch (this->data_type)
+//     {
+//     case INT_UNIT:
+//         cout<<"int: "<<this->value.int_value;
+//         break;
+//     case FLOAT_UNIT:
+//         cout<<"float:"<<this->value.float_value;
+//         break;
+//     case CHAR_UNIT:
+//         cout<<"char: "<<this->value.char_n_value;
+//         break;
+
+//     default:
+//         break;
+//     }
+//     cout<<endl;
+// }
+
+ConditionUnit::ConditionUnit(string attr_name, int attr_num, OpCode op_code, DataType data_type) : value()
+{
+    this->attr_name = attr_name;
+    this->attr_num = attr_num;
+    this->op_code = op_code;
+    this->data_type = data_type;
+}
+
+void ConditionUnit::Print()
+{
+    cout << "[Condition Unit]:\n"
+         << "attr name = \"" << this->attr_name << "\", Op = \"";
+    switch (this->op_code)
+    {
+    case EQ_:
+        cout << "=\"";
+        break;
+    case NE_:
+        cout << "!=\"";
+        break;
+    case LE_:
+        cout << "<=\"";
+        break;
+    case GE_:
+        cout << ">=\"";
+        break;
+    case L_:
+        cout << "<\"";
+        break;
+    case G_:
+        cout << ">\"";
+        break;
+    default:
+        break;
+    }
+    cout << ", value = \"";
+    switch (this->data_type)
+    {
+    case INT_UNIT:
+        cout << "int:" << this->value.int_value;
+        break;
+    case FLOAT_UNIT:
+        cout << "float:" << this->value.float_value;
+        break;
+    case CHAR_UNIT:
+        cout << "char:" << this->value.char_n_value;
+        break;
+
+    default:
+        break;
+    }
+    cout << "\"" << endl;
+}
