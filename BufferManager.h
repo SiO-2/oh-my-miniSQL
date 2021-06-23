3edea55d1a4d42c83a62b25dd58d2ef4bbf72c99
SiO2 <57137286+SiO-2@users.noreply.github.com>
2021-06-23 21:39:49 +0800

    优化注释

--- BufferManager.h
+++ BufferManager.h
@@ -87,18 +87,8 @@ public:
 class BufferManager
 {
 private:
-    /*
-        函数功能：进行BID的分配
-        传入参数：带路径的文件名，以及地址偏移
-        返回值：分配好的block的bid
-    */
     BID GetBlock(const string &filename, const unsigned int &offset) const;
 
-    /*
-        函数功能：将block初始化
-        传入参数：block的bid，带路径的文件名，block在文件中的偏移
-        返回值：没有返回值
-    */
     void SetBlockInfo(const BID &bid, const string &filename, const unsigned int &offset);
 
 public:
@@ -110,6 +100,7 @@ public:
 
     BufferManager(){};
 
+    //析构时将所有有过修改的block写回文件
     ~BufferManager()
     {
         for (BID bid = 0; bid < MAX_BLOCK_NUMBER; bid++)
@@ -119,32 +110,12 @@ public:
         // cout << "BufferManager.h>BufferManager>~BufferManager() finished" << endl;
     };
 
-    /*
-        函数功能：获取文件的大小
-        传入参数：带路径的文件名
-        返回值：文件大小，单位为Byte
-    */
     size_t GetFileSize(const std::string &filename) const;
 
-    /*
-        函数功能：将文件读入到blocks中
-        传入参数：文件名以及block在文件中的偏移量（偏移量可缺省）
-        返回值：vector<BID> bids，即文件对应的所有的block的bid
-    */
     vector<BID> ReadFile2Block(const string &filename, const vector<unsigned int> &offset = vector<unsigned int>());
 
-    /*
-        函数功能：将块写回文件
-        传入参数：传入参数为需要写回的块的bid
-        返回值：没有返回值
-    */
     void WriteBlock2File(const BID &bid);
 
-    /*
-        函数功能：当表被删除时，将对应的block清空
-        传入参数：被删除文件的带路径文件名
-        返回值：没有返回值
-    */
     void FlushBlock(const string &filename);
 };
 
--- RecordManager.h
+++ RecordManager.h
@@ -29,69 +29,34 @@ public:
 		delete bmanager;
 	};
 
-	/*
-		函数功能：根据表名得到存储表数据的文件名
-		传入参数：表名
-		返回值：存储表数据文件的文件名
-	*/
 	string GetDataFileName(const string tablename) const
 	{
 		// cout<<"[DataFileName Debug]:"<<TABLE_PATH<<" "<<tablename<<" "<<TABLE_SUFFIX<<endl;
 		return TABLE_PATH + tablename + TABLE_SUFFIX;
 	}
 
-	/*
-		函数功能：根据表名得到存储表index的文件名
-		传入参数：表名
-		返回值：存储表index文件的文件名
-	*/
 	string GetIndexFileName(const string tablename) const
 	{
 		return INDEX_PATH + tablename + INDEX_SUFFIX;
 	}
 
-	/*
-		函数功能：根据表名得到存储表元数据的文件名
-		传入参数：表名
-		返回值：存储表元数据文件的文件名
-	*/
 	string GetMetaFileName(const string tablename) const
 	{
 		return META_PATH + tablename + META_SUFFIX;
 	}
 
-	/*
-		函数功能：创建一个表数据文件
-		传入参数：Table类变量
-		返回值：没有返回值
-	*/
-	void CreateTableFile(const Table &table);
+	// void CreateTableFile(const Table &table);
 
-	/*
-		函数功能：清空被删除文件的block
-		传入参数：Table类变量
-		返回值：没有返回值
-	*/
-	void DropTableFile(const Table &table);
-
-	/*
-		函数功能：向表文件中插入元组，支持每次一条元组的插入操作
-		传入参数：表名称，元组变量
-		返回值：没有返回值
-	*/
-	// By wyc: 由于tuple len是新加的, catalog没适配，我这里获取不到，里面的那部分先注释掉了
+	// void DropTableFile(const Table &table);
 
 	void InsertTuple(const Table &table, const Tuple &tuple);
 	// void InsertTuple(const Table &table, const Tuple &tuple, Index & index);
 
 	bool ConditionTest(const Tuple &tuple, const vector<ConditionUnit> &condition = vector<ConditionUnit>()) const;
 
-	//数据查询，可以通过指定用and 连接的多个条件进行查询，支持等值查询和区间查询
-	// wyc：似乎所有写Select的人都忽略了还有Attr list这个东西，罢了，我在Interpreter里加吧
 	vector<Tuple> SelectTuple(const Table &table, const vector<ConditionUnit> &condition = vector<ConditionUnit>()) const;
 	// vector<Tuple> SelectTuple(const Table &table, const vector<ConditionUnit> &condition = vector<ConditionUnit>(), Index& index) const;
 
-	//删除元组，支持每次一条或多条记录的删除操作
 	void DeleteTuple(const Table &table, const vector<ConditionUnit> &condition = vector<ConditionUnit>());
 
 	unsigned int GetTuplelen(const Table &table) const;
