a46367172b6c0370d7d5ee5a4317a1facb062b44
SiO-2 <2586457570@qq.com>
2021-06-23 19:03:50 +0800

    添加了对recordmanager与catalogmanager类变量的delete

--- Interpreter.h
+++ Interpreter.h
@@ -5,17 +5,23 @@
 #include "RecordManager.h"
 using namespace std;
 // sql解释器
-class Interpreter{
+class Interpreter
+{
 public:
     string sql; // sql语句字符串
-    CatalogManager Cata;
     RecordManager Record;
+    CatalogManager Cata;
     Interpreter(std::string sql); //构造函数
-    void Parse(); // 解析sql的函数
+    ~Interpreter()
+    {
+        delete &Record;
+        delete &Cata;
+    }
+    void Parse();                      // 解析sql的函数
     void CreateTable(std::string str); // 针对create table 场景的函数
-    void CreateIndex(string str); // 针对create index 场景的函数
-    void Insert(string str); // 针对insert 场景的函数
-    void Select(string str); // 针对select 场景的函数
+    void CreateIndex(string str);      // 针对create index 场景的函数
+    void Insert(string str);           // 针对insert 场景的函数
+    void Select(string str);           // 针对select 场景的函数
     void DropTable(std::string str);
     void DropIndex(std::string str);
 };
